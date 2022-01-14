5a130acecf979cdb28479e625822e7bb8efac760
Pratyush <conenct2pratyush@gmail.com>
2022-01-14 16:05:44 +0530

    bugFix-1

--- popup.js
+++ popup.js
@@ -18,7 +18,32 @@ chrome.storage.sync.get(['freeLeft', 'uid', 'isBought'], function (result) {
 
     hpb = document.getElementById('havePaid');
     hpb.onclick = function(){
+        chrome.storage.sync.get(['freeLeft', 'uid'], function (result) {
+            fetch('https://vit-captcha-solver.herokuapp.com/api/', {
+            // fetch('https://http://127.0.0.1:5000/api/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({'function':'checkBought', 'uid':result.uid}),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data.ans) {
+                    chrome.storage.sync.set({isBought:1}, function(){
+                        console.log('Uid '+result.uid+' Bought');
+                    })
+                } else {
+                    chrome.storage.sync.set({isBought:0}, function(){
+                        console.log('Uid '+result.uid+' not Bought');
+                    })
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+        });
         chrome.runtime.reload()
     }
-
 });
