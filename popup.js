aec02e432e62868cfed77725c43da2ac11073490
Pratyush <conenct2pratyush@gmail.com>
2022-01-15 18:31:31 +0530

    rate us

--- popup.js
+++ popup.js
@@ -5,6 +5,9 @@ chrome.storage.sync.get(['freeLeft', 'uid', 'isBought'], function (result) {
     var a = document.getElementById("payblink");
     a.href = 'https://vit-captcha-solver.herokuapp.com/pay/?uid='+result.uid
 
+    var b = document.getElementById("rate");
+    b.href = 'https://chrome.google.com/webstore/detail/vit-captcha-solver/balpfhmdaaahhppiijcgaemeoeojejam/reviews'
+
     if (result.isBought){
         document.getElementById("content-bought").style.display='block';
         document.getElementById("content-no-bought").style.display='none';
